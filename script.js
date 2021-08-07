a486bbc0354b7144099159ced512d356bec1eaa5
santiago <arboledamartinezs222@gmail.com>
2021-08-06 19:50:10 -0500

    search input

--- script.js
+++ script.js
@@ -2,34 +2,53 @@
 const searchWrapper = document.querySelector(".search-input");
 const inputBox = document.querySelector("input");
 const suggBox = document.querySelector(".autocom-box");
-
+let searchUrl = "https://api.giphy.com/v1/gifs/search?api_key=bYQ1Vfn6G2NKpWha0cAgVmdsArrnZiSz&limit=12&offset=0&rating=g&lang=en&q=";
 
 // if user press any key and release
 inputBox.onkeyup = (e) => {
     let userData = e.target.value;//user entred data
     let emptyArray = [];
-    if(userData){
-        emptyArray = sugestions.filter((data)=>{
-            //filtering array value and user char to lowercase and return only those word sentc which are stars with user entered  word
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-            //console.log(emptyArray);
+    let searchSuggestions = [];
+    let url = searchUrl+userData;
+    console.log(url);
+    async function getFrames() {
+        let response = await fetch(url);
+        let responseJson = await response.json();
+        let data = responseJson.data;
+
+        data.forEach(e => {
+
+            searchSuggestions.push(e.title)
         });
-        emptyArray = emptyArray.map((data)=>{
-            return data = `<li>${data}</li>`;
-        });
-        console.log(emptyArray);
-        searchWrapper.classList.add("active");//show autocomplete div
-        showSuggestions(emptyArray);
-        let allList = suggBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //ADDING ONCLICK ATTRIBUTE  IN ALL LI TAG 
-            allList[i].setAttribute("onclick","select(this)");
-            
-        }
-    }else{
-        searchWrapper.classList.remove("active");//hide autocomplete div
-    }
+        console.log(searchSuggestions);
+        //return searchSuggestions;
+
+        if (userData) {
+            emptyArray = searchSuggestions.filter((data) => {
+                //filtering array value and user char to lowercase and return only those word sentc which are stars with user entered  word
+                return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+                //console.log(emptyArray);
+            });
+            emptyArray = emptyArray.map((data) => {
+                return data = `<li>${data}</li>`;
+            });
+            console.log(emptyArray);
+            searchWrapper.classList.add("active");//show autocomplete div
+            showSuggestions(emptyArray);
+            let allList = suggBox.querySelectorAll("li");
+            for (let i = 0; i < allList.length; i++) {
+                //ADDING ONCLICK ATTRIBUTE  IN ALL LI TAG 
+                allList[i].setAttribute("onclick", "select(this)");
     
+            }
+        } else {
+            searchWrapper.classList.remove("active");//hide autocomplete div
+        }
+    }
+   
+    getFrames();
+    
+
 
     /* console.log(e.target.value); */
 }
@@ -46,8 +65,9 @@ function showSuggestions(list) {
     if (!list.length) {
         userValue = inputBox.value;
         listData = `<li>${userValue}</li>`;
-    }else{
+    } else {
         listData = list.join('');
     }
     suggBox.innerHTML = listData;
 }
+
--- suggestions.js
+++ suggestions.js
@@ -1,4 +1,24 @@
-let sugestions = [
+/* const searchUrl = "https://api.giphy.com/v1/gifs/search?api_key=bYQ1Vfn6G2NKpWha0cAgVmdsArrnZiSz&limit=12&offset=0&rating=g&lang=en&q=";
+
+let searchSuggestions =[];
+
+
+async function getFrames() {
+    let response = await fetch(searchUrl);
+    let responseJson = await response.json();
+    let data = responseJson.data;
+    
+    data.forEach(e => {
+       
+        searchSuggestions.push(e.title)
+    });
+    console.log(searchSuggestions);
+    return searchSuggestions;
+}
+console.log(searchSuggestions);
+getFrames(); */
+
+/* let sugestions = [
     "FUCK YOU BITCH",
     "FUCK HOE",
     "FUCK BOY",
@@ -14,4 +34,4 @@ let sugestions = [
     "fuck la gente",
     "fuck la gente",
 
-];
+]; */
